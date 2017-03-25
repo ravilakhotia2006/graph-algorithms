7d2a592517a55f2d6bbae6c0d29777680e387cb1
ravilakhotia2006 <ravilakhotia2006@gmail.com>
2017-03-26 02:09:33 +0530

    edge initialize condition correction

--- representation/edge.rb
+++ representation/edge.rb
@@ -1,17 +1,16 @@
 require_relative '../error/error'
+require_relative './vertex'
 
 class Edge
-  
   attr_accessor :from, :to, :weight
 
   def initialize(from , to , weight = 0)
-
-    if !from.class.eql? Vertex or !to.class.eql? Vertex
+    if from.class.eql? Vertex and to.class.eql? Vertex
       @from = from
       @to = to
       @weight = weight
     else
-      raise VertexError, 'from and to should be of class Vertex'
+      raise VertexError, 'from and to should be an instance of Vertex'
     end
   end
 end
